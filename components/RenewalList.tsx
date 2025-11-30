8cf0b3fa92144edc6356c01ff623c3d7b4b3d3b2
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-11-29 23:08:08 -0300

    Update RenewalList.tsx

--- components/RenewalList.tsx
+++ components/RenewalList.tsx
@@ -74,10 +74,20 @@ const RenewalCard: React.FC<{ lead: Lead, onUpdate: (l: Lead) => void, onAdd: (l
     // Calculate End Date whenever Start Date changes
     useEffect(() => {
         if (dealForm.startDate) {
-            const start = new Date(dealForm.startDate);
-            const end = new Date(start);
-            end.setFullYear(end.getFullYear() + 1);
-            setDealForm(prev => ({ ...prev, endDate: end.toISOString().split('T')[0] }));
+            let dateStr = dealForm.startDate;
+            if (dateStr.includes('/')) {
+                const [d, m, y] = dateStr.split('/');
+                dateStr = `${y}-${m}-${d}`;
+            }
+
+            const start = new Date(dateStr);
+            if (!isNaN(start.getTime())) {
+                const end = new Date(start);
+                end.setFullYear(end.getFullYear() + 1);
+                try {
+                    setDealForm(prev => ({ ...prev, endDate: end.toISOString().split('T')[0] }));
+                } catch(e) { console.error(e) }
+            }
         }
     }, [dealForm.startDate]);
 
